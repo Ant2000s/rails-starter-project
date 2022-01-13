92e27262a1e9d414179be8c14ebe01e69eae85e9
Ant2000s <skrebnev.anton@gmail.com>
2022-01-13 13:26:05 +0700

    small fix for rubocop

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,20 +1,18 @@
 class PasswordsController < ApplicationController
-    before_action :require_user_logged_in!
-    def edit
+  before_action :require_user_logged_in!
+  def edit; end
 
+  def update
+    if Current.user.update(password_params)
+      redirect_to root_path, notice: 'Password updated!'
+    else
+      render :edit
     end
+  end
 
-    def update
-        if Current.user.update(password_params)
-            redirect_to root_path,notice: "Password updated!"
-        else
-            render :edit
-        end
-    end
+  private
 
-    private
-
-    def password_params
-        params.require(:user).permit(:password, :password_confirmation)
-    end
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }
+  validates :email, unique: true, presence: true,
+                    format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }
 end
