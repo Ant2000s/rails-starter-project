cc54b94b8188f59f203054cc26bdfb04a5bafa14
Ant2000s <skrebnev.anton@gmail.com>
2022-01-13 20:12:33 +0700

    test user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'create norm user' do
+    user = described_class.create(email: 'norm@email', password: '123')
+    expect(user).to be_valid
+  end
+
+  it 'create user with error in email' do
+    user = described_class.create(email: 'AbnormalEmail', password: '123')
+    expect(user).not_to be_valid
+  end
+
+  it 'create user with error in password' do
+    user = described_class.create(email: 'norm@email', password: '12')
+    expect(user).not_to be_valid
+  end
 end
