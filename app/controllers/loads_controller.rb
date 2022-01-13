8ee97acdf1a7ef99c645056a8dc27165b67282d1
GreyWayfarer <l.box@bk.ru>
2022-01-13 17:35:11 +0700

    Add download (some bugs)

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -8,6 +8,9 @@ class LoadsController < ApplicationController
 
   # GET /loads/1 or /loads/1.json
   def show
+    extension=@load.title.split('.')
+    send_file Rails.root.join('public', 'uploads', @load.title),
+    :type=>"application/#{extension[1]}", :x_sendfile=>true
   end
 
   # GET /loads/new
@@ -22,8 +25,12 @@ class LoadsController < ApplicationController
   # POST /loads or /loads.json
   def create
     @load = Load.new(load_params)
-
-    respond_to do |format|
+    uploaded_io = params[:load][:title]
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename),'wb') do |file|
+    file.write(uploaded_io.read)
+  end
+  @load.title=uploaded_io.original_filename
+      respond_to do |format|
       if @load.save
         format.html { redirect_to @load, notice: "Load was successfully created." }
         format.json { render :show, status: :created, location: @load }
