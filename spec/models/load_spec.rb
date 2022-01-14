b6a4b5606d7c0cc88deb57e8e36ac17f3dd997ad
Ant2000s <skrebnev.anton@gmail.com>
2022-01-14 21:15:36 +0700

    add test load

--- spec/models/load_spec.rb
+++ spec/models/load_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Load, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'create norm load' do
+    load = described_class.create(title: 'ruby.rb', user_id: 123)
+    expect(load).to be_valid
+  end
+
+  it 'the same user already has such a file in the database' do
+    described_class.create(title: 'ruby.rb', user_id: 123)
+    load = described_class.create(title: 'ruby.rb', user_id: 123)
+    expect(load).not_to be_valid
+  end
 end
